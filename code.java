064a14460b70f3b37b885c706ff31421d33a5c8f
Sushant kumar jha <72967077+sushant135@users.noreply.github.com>
2022-10-16 11:54:37 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,10 @@
+public class username {
+static void username(String email){
+String name = email.substring(0,a);
+System.out.println(name);
+
+    }
+    public static void main(String args[]){
+      username("sushant@gmail.com");
+    }
+}
